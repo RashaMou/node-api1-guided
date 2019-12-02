aea7bde08ea809e70384d77f20c5e77fd5495f94
Rasha Moumneh <rasha@fastmail.com>
2019-12-02 20:45:46 +0200

    Built first endpoint to get list of hubs

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express"); //CommonJS Modules
+const db = require("./data/hubs-model");
+
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send({ api: "up and running" });
+});
+
+const port = 4000;
+
+server.listen(port, () => {
+  console.log(`\n ** API running on port ${port} ** \n`);
+});
+
+server.get("/hubs", (req, res) => {
+  db.find()
+    .then(hubs => {
+      res.status(200).json(hubs);
+    })
+    .catch(error => {
+      console.log("error on /GET hubs", error);
+      res
+        .status(500)
+        .json({ errorMessage: "error getting list of hubs from database" });
+    });
+});
